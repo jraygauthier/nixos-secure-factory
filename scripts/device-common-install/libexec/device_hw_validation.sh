1e67100c261a08fbc2b7bdd006e01b426f6b6b29
Mathieu Tetreault <mathieu.alexandre.tetreault@gmail.com>
2020-01-21 09:13:14 -0500

    device-common-install: add device hw validation tool
    * Check device hardware prior to installing nixos

---
+++ scripts/device-common-install/libexec/device_hw_validation.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+is_expected_hdd() {
+  local expected_value
+  expected_value="${1?ERROR variables is unset}"
+
+  local tolerance
+  tolerance="${2?ERROR variable is unset}"
+
+  local lower_bound
+  hdd_lower_bound=$(echo "($1 - $2)" | bc -l)
+
+  local hdd_upper_bound
+  hdd_upper_bound=$(echo "($1 + $2)" | bc -l)
+
+  local hdd_size
+  hdd_size="$(lsblk /dev/sda | head -n 2 | tail -n 1 | awk '{print $4}' | sed 's/[A-Z]//g')"
+
+  if [ "1" -eq "$(echo "${hdd_size} >= ${hdd_lower_bound}" | bc -l)" ] && \
+     [ "1" -eq "$(echo "${hdd_size} <= ${hdd_upper_bound}" | bc -l)" ]; then
+     return 0
+  else
+    return 1
+  fi
+
+  # This point should never be reached.
+  return 1
+}
+
+is_expected_ram() {
+  local expected_value
+  expected_value="${1?ERROR variables is unset}"
+
+  local tolerance
+  tolerance="${2?ERROR variable is unset}"
+
+  local ram_lower_bound
+  ram_lower_bound=$(echo "($expected_value - $tolerance)" | bc -l)
+
+  local ram_upper_bound
+  ram_upper_bound=$(echo "($expected_value + $tolerance)" | bc -l)
+
+  local ram_size
+  ram_size="$(free -m --si | grep Mem: | awk '{print $2}')"
+  if [ 1 -eq $(echo "${ram_size} >= ${ram_lower_bound}" | bc -l) ] && \
+     [ 1 -eq $(echo "${ram_size} <= ${ram_upper_bound}" | bc -l) ]; then
+     return 0
+  else
+    return 1
+  fi
+
+  # This point should never be reached.
+  return 1
+}
+
+ensure_expected_device() {
+  local hw_config_yaml_str
+  hw_config_yaml_str="${1?ERROR variables is unset}"
+
+  local network_interface
+  network_interface="${2?ERROR variable is unset}"
+
+  local device
+  device="${3?ERROR variable is unset}"
+
+  local device_mac
+  device_mac=$(ifconfig "$network_interface" | grep ether | awk '{print $2}')
+
+  local key
+  if ! echo "$hw_config_yaml_str" | yq -e --arg key "$device_mac" '.[$key]' > /dev/null; then
+    key="default"
+  else
+    key="$device_mac"
+  fi
+
+  local expected_value
+  expected_value="$(echo "$hw_config_yaml_str" | yq -e -j --arg key "$key" \
+                                           --arg dev "$device" \
+                                           '.[$key][$dev].size')"
+
+  local tolerance
+  tolerance="$(echo "$hw_config_yaml_str" | yq -e -j --arg key "$key" \
+                                      --arg dev "$device" \
+                                      '.[$key][$dev].tolerance')"
+
+  if [ null == "$expected_value" ] || [ null == "$tolerance" ];then
+    1>&2 printf "ERROR: Could not read specification from \n $hw_config_yaml_str \n"
+    return 1
+  fi
+
+  if [ "$device" == ram ]; then
+    if ! is_expected_ram "$expected_value" "$tolerance"; then
+      1>&2 echo "ERROR: $device size is out of range, expecting $expected_value"
+      return 1
+    fi
+  else
+    if ! is_expected_hdd "$expected_value" "$tolerance"; then
+      1>&2 echo "ERROR: $device size is out of range, expecting $expected_value"
+      return 1
+    fi
+  fi
+}
--- scripts/device-common-install/libexec/tools.sh
+++ scripts/device-common-install/libexec/tools.sh
@@ -4,3 +4,6 @@ device_common_install_libexec_dir="$(pkg-nixos-device-common-install-get-libexec
 
 common_install_libexec_dir="$(pkg-nixos-common-install-get-libexec-dir)"
 . "$common_install_libexec_dir/prettyprint.sh"
+
+device_common_install_libexec_dir="$(pkg-nixos-device-common-install-get-libexec-dir)"
+. "$device_common_install_libexec_dir/device_hw_validation.sh"
