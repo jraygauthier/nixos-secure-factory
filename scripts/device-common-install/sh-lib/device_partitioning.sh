e21c3623ede444f7421f29be6af41a4fe2242bfe
Raymond Gauthier <jraygauthier@gmail.com>
2022-09-22 00:06:03 -0400

    device-common-install: fix part / format tools

--- scripts/device-common-install/sh-lib/device_partitioning.sh
+++ scripts/device-common-install/sh-lib/device_partitioning.sh
@@ -4,7 +4,10 @@ get_device_partition_by_1b_index() {
   declare device="${1?}"
   declare partition_1b_index="${2?}"
 
-  if echo "$device" | grep -q -E '^nvme'; then
+  declare device_bn
+  device_bn=$(basename "${device}")
+
+  if echo "$device_bn" | grep -q -E '^nvme'; then
     # New naming scheme for SDD devices.
     echo "${device}p${partition_1b_index}"
   else
